2c305a17d9b865dea5965ae9b1909145464bc46d
barcelosopivo <105869041+barcelosopivo@users.noreply.github.com>
2024-08-12 09:36:49 -0300

    Add files via upload

---
+++ aleatorio.js
@@ -0,0 +1,8 @@
+const nomes = ["Emanoel", "Emilly", "Kemilly"];
+
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+
+export const nome = aleatorio(nomes)
